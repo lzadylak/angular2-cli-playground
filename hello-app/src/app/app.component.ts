0fddd169162347ef8a11a9e8c8d3b1bbe64db329
Lukasz <miyek33@gmail.com>
2017-01-11 19:44:05 +0100

    add firebase and shared components header, sidebar

--- hello-app/src/app/app.component.ts
+++ hello-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  constructor(public af: AngularFire) { }
+
+  login() {
+    this.af.auth.login({
+      email: "kstw92@gmail.com",
+      password: "test12345"
+    })
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  ngOnInit() { }
+
 }
--- hello-app/src/app/app.module.ts
+++ hello-app/src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
-
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+export const myFirebaseConfig = {
+  apiKey: "AIzaSyBKP4cOP508h0JLKmjFvzJooO0MqV8l4fU",
+  authDomain: "project-5613440220430148247.firebaseapp.com",
+  databaseURL: "https://project-5613440220430148247.firebaseio.com",
+  storageBucket: "project-5613440220430148247.appspot.com",
+  messagingSenderId: "122242095723"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SharedModule,
+    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
